6cafd74a9fbb24ea14e381edf18f2eaf7138d4a1
mokrovalexander <94866168+Pequ0D0@users.noreply.github.com>
2022-02-26 12:24:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,16 @@
   <div class="row g-2">
     <div class="col-6">
       <div class="p-3 ">    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+       
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="assets/car/1.jpg" class="d-block w-100" alt="...">
     </div>
+   
     <div class="carousel-item">
       <img src="assets/car/2.jpg" class="d-block w-100" alt="..." >
     </div>
+   
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
